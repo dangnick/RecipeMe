62e5dd23bb191377ac915a0081da2a11ade19ac3
dangnick <dang.nicholas@hotmail.com>
2025-03-10 22:17:01 -0500

    Initial Commit

---
+++ RecipeMe/RecipeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMeApp.swift
+//  RecipeMe
+//
+//  Created by Nicholas Dang on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
